082599b7c732ad5388034a8e95501f44ea50ecdc
terahashi <tkshplant@outlook.jp>
2026-03-11 23:00:35 +0900

    page.tsxを修正 TypeScriptにコード変更、完了。 JavaScriptからTypeScript化

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,24 @@ import Image from 'next/image';
 import { client } from '../libs/microcms';
 import Link from 'next/link';
 
-////「記事一覧」を取得
+////「TypeScript」ブログ記事の型エイリアスを定義する。
+type Blog = {
+  id: string; //記事ID
+  title: string; //記事タイトル
+  thumbnail: {
+    url: string; //サムネイル画像URL
+  };
+  publishedAt: string; //投稿日時
+};
+
+////「記事一覧」を取得。
 //SSG(静的生成)を使ってデータを取得します。
 //なぜSSG(静的生成)を使う？➡︎『microCMS APIをビルド時に取得し、Next.jsでHTMLを生成するから』です。
-async function getBlogPosts() {
+async function getBlogPosts(): Promise<Blog[]> {
+  //⬆︎:Promise<Blog[]>は「戻り値の型」を明示している。
+  //意味:「このgetBlogpots関数は Promiseが戻り値で、中身は<Blog[]>」になる。
+  //Blog[]の意味:「Blog型オブジェクトの配列」という意味。つまり記事データの配列が配列が返ってくる。
+  //async関数は必ずPromiseを返すので、Promise<型>をと書くべし。
   const data = await client.get({
     endpoint: 'blog', //'blog'はエンドポイント名。
     queries: {
@@ -22,9 +36,9 @@ async function getBlogPosts() {
   return data.contents; //取得した記事一覧を返却する。contentsは「APIから取得した"contentsプロパティ(記事配列)"」
 }
 
-////トップページ
+////トップページ(記事一覧を表示)
 export default async function Home() {
-  //⬇︎getBlogPostsでmicroCMSから取得した「記事一覧」をpostsに格納する。
+  //⬇︎await getBlogPosts()で「microCMSから取得した『記事一覧』」をpostsに格納する。
   const posts = await getBlogPosts();
 
   return (
@@ -37,9 +51,12 @@ export default async function Home() {
         {/* ⬇︎取得した記事一覧(posts)を「mapメソッド」で表示する。(ちなみに"配列なので"mapが使用できます) */}
         <div>
           <ul>
-            {posts.map((post: any) => (
+            {posts.map((post) => (
               <li key={post.id}>
-                <Link href={`/blog/${post.id}`}>{post.title}</Link>
+                <Link href={`/blog/${post.id}`}>
+                  {/* 記事タイトルを表示する */}
+                  {post.title}
+                </Link>
               </li>
             ))}
           </ul>
