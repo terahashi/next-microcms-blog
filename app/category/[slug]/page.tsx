4bb4c5f0acbe6953a0cb803a9b038c9a694fa3d1
terahashi <tkshplant@outlook.jp>
2026-03-16 01:53:15 +0900

    category/[slug]/page.tsx 仮置きページを作成（API呼び出し未実装）

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -5,3 +5,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 ////「カテゴリ一覧ページ」を表示
+export default function CategoryPage() {
+  return (
+    <div>
+      <h1>カテゴリの記事一覧</h1>
+      <p>記事はまだありません（仮置き）</p>
+    </div>
+  );
+}
